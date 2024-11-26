bf018ba6408f23899dae3bf18970705b6461e86f
waseemR02 <waseem.riaz.9999@gmail.com>
2024-11-26 19:41:52 +0530

    use get() instead of indexing for odd number of bytes
    Signed-off-by: waseemR02 <waseem.riaz.9999@gmail.com>

--- src/dump.rs
+++ src/dump.rs
@@ -1,7 +1,30 @@
 use std::fs;
 
 pub fn disasm(filepath: String) -> Result<(), Box<dyn std::error::Error>> {
-    let buffer = fs::read(filepath)?;
-    println!("Buffer: {buffer:02X?}");
+    let buffer = fs::read(&filepath)?;
+    let mut pc: u16 = 0x200;
+
+    println!("Length: {}", buffer.len());
+    println!("Disassembly of {}:\n", &filepath);
+    for i in (0..buffer.len()).step_by(2) {
+        println!(
+            "  {pc:04X}:\t\t {:02X} {:02X}",
+            buffer.get(i).unwrap(),
+            buffer.get(i + 1).unwrap_or_else(|| &0x00),
+        );
+        pc += 0x02;
+    }
+    //println!("Buffer: {buffer:02X?}");
     Ok(())
 }
+
+//pub fn decode(code: &u16, pc: u16) -> &u16 {
+//    let mut f_nibble = code & 0xF0;
+//    f_nibble = f_nibble >> 12;
+//    match f_nibble {
+//        0x0 => {}
+//        _ => {}
+//    }
+//
+//    code
+//}
