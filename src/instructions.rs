0e749da3a66a30ea4c48d2656f2a1a9babd9d064
waseemR02 <waseem.riaz.9999@gmail.com>
2024-11-28 01:20:25 +0530

    Add tests for instruction type
    Signed-off-by: waseemR02 <waseem.riaz.9999@gmail.com>

--- src/instructions.rs
+++ src/instructions.rs
@@ -21,3 +21,56 @@ impl Instruction {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Instruction;
+
+    #[test]
+    fn test_nnn() {
+        let instruct = Instruction::new(&[0x12, 0x28]);
+
+        assert_eq!(instruct.opcode, 0x1228);
+        assert_eq!(instruct.nnn, 0x228);
+    }
+
+    #[test]
+    fn test_x() {
+        let instruct = Instruction::new(&[0x43, 0xE0]);
+
+        assert_eq!(instruct.opcode, 0x43E0);
+        assert_eq!(instruct.x, 0x3);
+    }
+
+    #[test]
+    fn test_y() {
+        let instruct = Instruction::new(&[0x51, 0x20]);
+
+        assert_eq!(instruct.opcode, 0x5120);
+        assert_eq!(instruct.y, 0x2);
+    }
+
+    #[test]
+    fn test_n() {
+        let instruct = Instruction::new(&[0xD3, 0x42]);
+
+        assert_eq!(instruct.opcode, 0xD342);
+        assert_eq!(instruct.l_nibble, 0x2);
+    }
+
+    #[test]
+    fn test_f_nibble() {
+        let instruct = Instruction::new(&[0xD3, 0x42]);
+
+        assert_eq!(instruct.opcode, 0xD342);
+        assert_eq!(instruct.f_nibble, 0xD);
+    }
+
+    #[test]
+    fn test_nn() {
+        let instruct = Instruction::new(&[0x32, 0x54]);
+
+        assert_eq!(instruct.opcode, 0x3254);
+        assert_eq!(instruct.nn, 0x54);
+    }
+}
